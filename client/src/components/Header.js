54d3122ac92f651125163c495e76d627cbc3ee14
FauzanKhan <fauzankhan93@gmail.com>
2017-11-04 22:22:31 +0100

    renamed app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => (
   <header>
     <nav className="clearfix">
-      <div className="float-left"><h3>Word<b>Book</b></h3></div>
+      <div className="float-left"><h3>Voca<b>Book</b></h3></div>
       <div className="float-right">
         <Link to="/categories" style={{padding: '0 20px', fontSize: '20px'}}>Categories</Link>
         <Link to="/words" style={{fontSize: '20px'}}>Words</Link>
