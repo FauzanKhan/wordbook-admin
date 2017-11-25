5c724aa515002b54f5bc168c25919f32f74e208f
FauzanKhan <fauzankhan93@gmail.com>
2017-11-26 00:37:55 +0100

    updated lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
         "react/jsx-filename-extension": 0,
         "react/sort-comp": 0,
         "object-curly-newline": 0,
+        "jsx-a11y/media-has-caption": 0,
     },
     "env": {
         "browser": true,
